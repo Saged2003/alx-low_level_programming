382646aad248ec338df377f982f82aa9ff8e0cc2
Saged2003 <sagedryan775@gmail.com>
2023-09-12 05:21:04 -0700

    saged

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - prints _putchar, followed by a new line
@@ -7,12 +7,14 @@
  */
 int main(void)
 {
-	char s[] = "_putchar\n";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(s[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
