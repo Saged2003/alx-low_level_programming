4916402054b61cf2c042316db73f9f62225f359c
Saged2003 <sagedryan775@gmail.com>
2023-09-14 11:08:14 -0700

    saged

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,13 +6,17 @@
  */
 int main(void)
 {
-	int n, sum = 0;
+	int n, sum;
 
+	sum = 0;
 	for (n = 0; n < 1024; n++)
 	{
 		if (n % 3 == 0 || n % 5 == 0)
+		{
 			sum += n;
+		}
 	}
 	printf("%d", sum);
 	printf("\n");
+	return (0);
 }
