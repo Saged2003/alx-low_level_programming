ae53bfe3a8f7707e1933258cf014ddb3edc636af
Saged2003 <sagedryan775@gmail.com>
2023-09-21 06:38:12 -0700

    saged

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - prints a[2] = 98, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int n;
@@ -7,18 +13,13 @@ int main(void)
 	int *p;
 
 	a[2] = 1024;
-	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
+	p = malloc(5 * sizeof(int));
+	p[0] = 98;
+	p[1] = 198;
+	p[2] = 298;
+	p[3] = 398;
+	p[4] = 498;
+	*(a + 2) = *p; /* This is the line of code that I have added */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
-
