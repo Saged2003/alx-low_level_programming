e7fe91d004ffc6064edefa0126757b88a7888474
Saged2003 <sagedryan775@gmail.com>
2023-09-24 14:11:14 -0700

    saged

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,9 +14,16 @@ void puts_half(char *str)
 	while (str[i] != '\0')
 	{
 		len++;
-		i++;
 	}
 
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len - 1) / 2 + 1;
+	}
 	for (i = n; i < len; i++)
 	{
 		_putchar(str[i]);
