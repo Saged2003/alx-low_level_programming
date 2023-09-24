5997c404a81e03d404ee729ee0089b721890692b
Saged2003 <sagedryan775@gmail.com>
2023-09-24 07:17:42 -0700

    saged

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,21 +2,17 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: string to evaluate
- *
+ * @s: a pointer to a char
  * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int i;
+	int len = 0;
 
-	i = 0;
-
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		i++;
+		len++;
+		s++;
 	}
-
-	return (i);
+	return (len);
 }
-
