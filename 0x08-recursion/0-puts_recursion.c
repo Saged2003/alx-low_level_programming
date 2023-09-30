369fb0528e1bafa232c5d2a00a81be68d81c6137
Saged2003 <sagedryan775@gmail.com>
2023-09-30 07:19:47 -0700

    saged

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,14 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line
- * @s: pointer to the string
- * Return: void
+ * @s: the string to print
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if(*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
 	else
 	{
