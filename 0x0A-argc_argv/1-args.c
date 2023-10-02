7ca2de548f5d53231ec7490963c20b4eeb20e701
Saged2003 <sagedryan775@gmail.com>
2023-10-02 10:01:00 -0700

    saged

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments
+ * main - program that prints the number of arguments passed into it
  * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 if successful
- */
+ * @argv: array with the arguments
+ *
+ * Return: always 0
+ **/
+
 int main(int argc, char *argv[])
 {
+	(void)argv;
+
 	printf("%d\n", argc - 1);
 	return (0);
 }
