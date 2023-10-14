3c61555b7c6eaeaaa60ab8c92a6b1e98e1e6529a
Saged2003 <sagedryan775@gmail.com>
2023-10-14 06:52:38 -0700

    saged

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,8 +10,9 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list lists;
-	unsigned int i, sum = 0;
+	va_list args;
+	unsigned int i;
+	unsigned int sum = 0;
 
 	if (n == 0)
 	{
@@ -20,8 +21,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(lists, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(lists, int);
-	va_end(lists);
+		sum += va_arg(args, int);
+	va_end(args);
 	return (sum);
 }
 
