bb0db13b2221bad1801ee55bd5a22c968b125080
Saged2003 <sagedryan775@gmail.com>
2023-10-25 13:34:40 -0700

    saged

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,29 +13,26 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	char *dup_str;
 	int len;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
+	{
 		return (NULL);
-
-	dup_str = strdup(str);
-	if (dup_str == NULL)
+	}
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-
-	for (len = 0; str[len];)
+	len = 0;
+	while (str[len] != "\0")
+	{
 		len++;
-
-	new->str = dup_str;
-	new->len = len;
-	new->next = *head;
-
-	*head = new;
-
-	return (new);
+		n->len = len;
+		n->next = *head;
+		*head = new;
+		return (new);
+	}
 }
-
