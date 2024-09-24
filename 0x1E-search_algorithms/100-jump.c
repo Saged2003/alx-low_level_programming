31b986fac44a0e55d905f01a47d71127490cd70a
Saged2003 <sagedryan775@gmail.com>
2024-09-24 08:47:23 -0700

    saged

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,45 @@
+
+#include "search_algos.h"
+
+/**
+ * jump_search - doing linear search
+ * @array: pointer to array
+ * @size: size of array
+ * @value: value to search
+ *
+ * Return: the index found or -1 if not found
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, j, m;
+
+	if (!array)
+		return (-1);
+
+	i = 0, m = sqrt(size);
+
+	if (array[i] > value)
+		return (-1);
+
+	while (i < size && array[i] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		i += m;
+	}
+	j = i;
+	i -= m;
+	printf("Value found between indexes [%lu] and [%lu]\n", i, j);
+
+	if (j > size)
+		j = size;
+
+	for (i = i; i < j; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
+
